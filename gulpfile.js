8738fafa390c0e3c2add32564c5068d4bb34b4c6
Tan Dang <tan@Tans-MacBook-Pro.local>
2020-04-12 17:25:13 +0200

    chore(gulp): update to v4, ignore dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,67 @@
-'use strict';
-
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var connect = require('gulp-connect');
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+const sass = require('gulp-sass');
+const connect = require('gulp-connect');
+// const terser = require('gulp-terser');
+const del = require('del');
+// const cleanCSS = require('gulp-clean-css');
 
 sass.compiler = require('node-sass');
 
-gulp.task('sass', function() {
+function clean() {
+  return del([
+    // here we use a globbing pattern to match everything inside the `mobile` folder
+    'dist/**/*',
+  ]);
+};
+
+function copyUploads() {
+    return gulp.src('./src/uploads/*')
+      .pipe(gulp.dest('./dist/uploads'));
+  }
+
+function compileScss () {
     return gulp.src('./src/**/index.scss')
         .pipe(sass({
             includePaths: ['node_modules']
         }).on('error', sass.logError))
         .pipe(gulp.dest('./dist'));
-});
+};
 
-gulp.task('sass:watch', function() {
-    gulp.watch('./src/**/*.scss', gulp.series('sass'));
-});
+function watchScss() {
+    gulp.watch('./src/**/*.scss', gulp.series(compileScss));
+};
 
-gulp.task('js', function() {
+function buildJavaScript() {
     return gulp.src('./src/**/index.js')
         .pipe(gulp.dest('./dist'));
-});
+};
 
-gulp.task('js:watch', function() {
-    gulp.watch('./src/**/*.js', gulp.series('js'));
-});
+function watchJavaScript () {
+    gulp.watch('./src/**/*.js', gulp.series(buildJavaScript));
+}
 
-gulp.task('connect', function() {
-  connect.server();
-});
+function startServer () {
+    connect.server();
+}
 
-// gulp.task('dev', ['connect', 'js:watch', 'sass:watch']);
+exports.build = series(
+    clean,
+    copyUploads,
+    buildJavaScript,
+    compileScss
+);
+
+exports.dev = series(
+    clean,
+    copyUploads,
+    buildJavaScript,
+    compileScss,
+    startServer,
+    parallel(watchJavaScript, watchScss)
+);
+
+// exports.prod = series(
+//     clean,
+//     copyUploads
+// );
